55a724b4747ad3b87fd18f6e34eaa176b5187a65
Yashu <yashuravindra26@gmail.com>
2026-08-13 23:24:29 +0530

    02-Component

---
+++ 02-Components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Yashu</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, nihil.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ 02-Components/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="nav">
+        This Is Nav
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
