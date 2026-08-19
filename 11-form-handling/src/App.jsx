292bf5cd0b3989855b2f19a03be13fc91ff00cd2
Yashu <yashuravindra26@gmail.com>
2026-08-19 23:24:59 +0530

    11-form-handling

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+
+  let submitHandler=(e)=>{
+    e.preventDefault();
+    console.log("Submitted");
+    
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input type="text" placeholder="Enter"/>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
